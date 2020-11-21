ee3f451d524e5690d19e58f6c015cba67e02421f
ksmin23 <sonagy23@gmail.com>
2020-11-22 00:34:48 +0900

    [WIP] add ecs to awslogs

--- cdk/app.py
+++ cdk/app.py
@@ -10,9 +10,11 @@ from aws_cdk import (
     aws_ecr,
     aws_ecs,
     aws_ecs_patterns,
+    aws_elasticache,
     aws_events,
     aws_events_targets,
     aws_iam,
+    aws_logs,
     aws_s3 as s3,
     aws_applicationautoscaling
 )
@@ -27,55 +29,62 @@ class RssFeedTransBotEcsStack(core.Stack):
     # The code that defines your stack goes here
     vpc_name = self.node.try_get_context("vpc_name")
     vpc = aws_ec2.Vpc.from_lookup(self, "VPC",
-      is_default=False,
+      is_default=True,
       vpc_name=vpc_name)
 
     s3_bucket_name = self.node.try_get_context('s3_bucket_name')
     s3_bucket = s3.Bucket.from_bucket_name(self, id, s3_bucket_name)
+
+    sg_use_elasticache = aws_ec2.SecurityGroup(self, 'RssFeedTransBotCacheClientSG',
+      vpc=vpc,
+      allow_all_outbound=True,
+      description='security group for redis client used rss feed trans bot',
+      security_group_name='use-rss-feed-trans-bot-redis'
+    )
+    core.Tags.of(sg_use_elasticache).add('Name', 'use-rss-feed-trans-bot-redis')
+
+    sg_elasticache = aws_ec2.SecurityGroup(self, 'RssFeedTransBotCacheSG',
+      vpc=vpc,
+      allow_all_outbound=True,
+      description='security group for redis used rss feed trans bot',
+      security_group_name='rss-feed-trans-bot-redis'
+    )
+    core.Tags.of(sg_elasticache).add('Name', 'rss-feed-trans-bot-redis')
+
+    sg_elasticache.add_ingress_rule(peer=sg_use_elasticache, connection=aws_ec2.Port.tcp(6379), description='use-rss-feed-trans-bot-redis')
+
+    elasticache_subnet_group = aws_elasticache.CfnSubnetGroup(self, 'RssFeedTransBotCacheSubnetGroup',
+      description='subnet group for rss-feed-trans-bot-redis',
+      subnet_ids=vpc.select_subnets(subnet_type=aws_ec2.SubnetType.PRIVATE).subnet_ids,
+      cache_subnet_group_name='rss-feed-trans-bot-redis'
+    )
+
+    translated_feed_cache = aws_elasticache.CfnCacheCluster(self, 'RssFeedTransBotCache',
+      cache_node_type='cache.t3.small',
+      num_cache_nodes=1,
+      engine='redis',
+      engine_version='5.0.5',
+      auto_minor_version_upgrade=False,
+      cluster_name='rss-feed-trans-bot-redis',
+      snapshot_retention_limit=3,
+      snapshot_window='17:00-19:00',
+      preferred_maintenance_window='mon:19:00-mon:20:30',
+      #XXX: Do not use referece for 'cache_subnet_group_name' - https://github.com/aws/aws-cdk/issues/3098
+      cache_subnet_group_name=elasticache_subnet_group.cache_subnet_group_name, # Redis cluster goes to wrong VPC
+      vpc_security_group_ids=[sg_elasticache.security_group_id]
+    )
+
+    #XXX: If you're going to launch your cluster in an Amazon VPC, you need to create a subnet group before you start creating a cluster.
+    # https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-elasticache-cache-cluster.html#cfn-elasticache-cachecluster-cachesubnetgroupname
+    translated_feed_cache.add_depends_on(elasticache_subnet_group)
 
     cluster = aws_ecs.Cluster(self, "ECSCluster",
       cluster_name="rssfeed-trans-bot",
       vpc=vpc
     )
 
-    # task_role_policy_doc = aws_iam.PolicyDocument()
-    # task_role_policy_doc.add_statements(aws_iam.PolicyStatement(**{
-    #   "effect": aws_iam.Effect.ALLOW,
-    #   "resources": [s3_bucket.bucket_arn, "{}/*".format(s3_bucket.bucket_arn)],
-    #   "actions": ["s3:AbortMultipartUpload",
-    #     "s3:GetBucketLocation",
-    #     "s3:GetObject",
-    #     "s3:ListBucket",
-    #     "s3:ListBucketMultipartUploads",
-    #     "s3:PutObject"]
-    # }))
-
-    # task_role_policy_doc.add_statements(aws_iam.PolicyStatement(**{
-    #   "effect": aws_iam.Effect.ALLOW,
-    #   "resources": ["*"],
-    #   "actions": ["ses:SendEmail"]
-    # }))
-
-    task_execution_role = aws_iam.Role(self, 'ecsScheduledTaskRole',
-      role_name='ecsRssFeedTransTaskExecutionRole',
-      assumed_by=aws_iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
-      managed_policies=[
-        aws_iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AmazonECSTaskExecutionRolePolicy")
-      ]
-    )
-
-    #XXX: ECS Fargate Task Scheduling using existing Security Group #5213
-    # https://github.com/aws/aws-cdk/issues/5213
-    # https://stackoverflow.com/questions/59067514/aws-cdk-ecs-task-scheduling-specify-existing-securitygroup
-    task = aws_ecs.FargateTaskDefinition(self, 'TaskDef',
-      cpu=512,
-      memory_limit_mib=1024,
-      execution_role=task_execution_role #TODO: can not attach Execution Role
-      #,task_role=task_role
-    )
-    #XXX: execution_role does not be created
-
-    task.add_to_task_role_policy(aws_iam.PolicyStatement(**{
+    task_role_policy_doc = aws_iam.PolicyDocument()
+    task_role_policy_doc.add_statements(aws_iam.PolicyStatement(**{
       "effect": aws_iam.Effect.ALLOW,
       "resources": [s3_bucket.bucket_arn, "{}/*".format(s3_bucket.bucket_arn)],
       "actions": ["s3:AbortMultipartUpload",
@@ -86,11 +95,28 @@ class RssFeedTransBotEcsStack(core.Stack):
         "s3:PutObject"]
     }))
 
-    task.add_to_task_role_policy(aws_iam.PolicyStatement(**{
-      "effect": aws_iam.Effect.ALLOW,
-      "resources": ["*"],
-      "actions": ["ses:SendEmail"]
-    }))
+    task_execution_role = aws_iam.Role(self, 'ecsScheduledTaskRole',
+      role_name='ecsRssFeedTransTaskExecutionRole',
+      assumed_by=aws_iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
+      inline_policies = {
+        "s3access": task_role_policy_doc
+      },
+      managed_policies=[
+        aws_iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AmazonECSTaskExecutionRolePolicy"),
+        aws_iam.ManagedPolicy.from_aws_managed_policy_name("AmazonSESFullAccess")
+      ]
+    )
+
+    #XXX: ECS Fargate Task Scheduling using existing Security Group #5213
+    # https://github.com/aws/aws-cdk/issues/5213
+    # https://stackoverflow.com/questions/59067514/aws-cdk-ecs-task-scheduling-specify-existing-securitygroup
+    task = aws_ecs.FargateTaskDefinition(self, 'TaskDef',
+      cpu=512,
+      memory_limit_mib=1024,
+      # execution_role=task_execution_role #TODO: can not attach Execution Role
+      task_role=task_execution_role
+    )
+    #XXX: execution_role does not be created
 
     repository_arn = aws_ecr.Repository.arn_for_local_repository(
       "transbot/rssfeed",
@@ -107,14 +133,19 @@ class RssFeedTransBotEcsStack(core.Stack):
       repository_name="transbot/rssfeed")
 
     task.add_container('ContainerImg',
-      image=aws_ecs.ContainerImage.from_ecr_repository(repository, tag="0.1"),
+      image=aws_ecs.ContainerImage.from_ecr_repository(repository, tag="0.3"),
       environment={
-        "ELASTICACHE_HOST": "localhost",
-        "DRY_RUN": "false"
-      }
+        "ELASTICACHE_HOST": translated_feed_cache.attr_redis_endpoint_address,
+        "DRY_RUN": "false",
+        "TRANSLATE_ALL_FEEDS": "false"
+      },
+      logging=aws_ecs.LogDriver.aws_logs(stream_prefix="ecs",
+        log_group=aws_logs.LogGroup(self, 
+          "ECSContainerLogGroup",
+          log_group_name="/ecs/rss-feed-trans-bot",
+          retention=aws_logs.RetentionDays.ONE_DAY)
+      )
     )
-    #TODO: how to create logging
-    # https://stackoverflow.com/questions/59838175/how-to-create-logging-for-ecs-task-definition
 
     # See https://docs.aws.amazon.com/lambda/latest/dg/tutorial-scheduled-events-schedule-expressions.html
     event_schedule = dict(zip(['minute', 'hour', 'month', 'week_day', 'year'],
@@ -123,16 +154,6 @@ class RssFeedTransBotEcsStack(core.Stack):
     scheduled_event_rule = aws_events.Rule(self, 'RssFeedScheduledRule',
       enabled=False,
       schedule=aws_events.Schedule.cron(**event_schedule))
-
-    sg_use_elasticache = aws_ec2.SecurityGroup.from_security_group_id(self, "RedisClientSG",
-      security_group_id="sg-0e187e1b14b2a01d0", # use-default-redis, sg-01234
-      mutable=False
-    )
-
-    sg_ecs_cron_task = aws_ec2.SecurityGroup.from_security_group_id(self, "EcsCronTaskSG",
-      security_group_id="sg-0ba7c0fc2c05c55d1", # cron-r-8382, sg-01234
-      mutable=False
-    )
 
     ecs_events_role = aws_iam.Role(self, 'ecsEventsRole',
       role_name='ecsRssFeedTransEventsRole',
@@ -145,35 +166,8 @@ class RssFeedTransBotEcsStack(core.Stack):
     scheduled_event_rule.add_target(aws_events_targets.EcsTask(cluster=cluster,
       task_definition=task,
       role=ecs_events_role,
-      security_groups=[sg_ecs_cron_task, sg_use_elasticache],
+      security_groups=[sg_use_elasticache],
       subnet_selection=aws_ec2.SubnetSelection(subnet_type=aws_ec2.SubnetType.PRIVATE)))
-
-    # task_def = aws_ecs.FargateTaskDefinition(self, "SchFargateTaskDef", **{
-    #   "cpu": 512,
-    #   "memory_limit_mib": 1024,
-    #   "task_role": task_role_role
-    # })
-    # t = aws_ecs_patterns.ScheduledFargateTaskDefinitionOptions(task_definition=task_def)
-
-    #XXX: ECS Fargate Task Scheduling using existing Security Group #5213
-    # https://github.com/aws/aws-cdk/issues/5213
-    # https://stackoverflow.com/questions/59067514/aws-cdk-ecs-task-scheduling-specify-existing-securitygroup
-    # ecs_scheduled_task = aws_ecs_patterns.ScheduledFargateTask(self, "ScheduledTask",
-    #   cluster=cluster,
-    #   # scheduled_fargate_task_definition_options=t,
-    #   scheduled_fargate_task_image_options={
-    #     #https://github.com/aws/aws-cdk/issues/3646
-    #     "image": aws_ecs.ContainerImage.from_ecr_repository(repository, tag="0.1"),
-    #     "cpu": 512,
-    #     "memory_limit_mib": 1024,
-    #     "environment": {
-    #       "ELASTICACHE_HOST": "localhost",
-    #       "DRY_RUN": "false"
-    #     }
-    #   },
-    #   schedule=aws_applicationautoscaling.Schedule.cron(minute="0/15"),
-    #   subnet_selection=aws_ec2.SubnetSelection(subnet_type=aws_ec2.SubnetType.PRIVATE),
-    #   vpc=vpc)
 
 _env = core.Environment(
   account=os.environ["CDK_DEFAULT_ACCOUNT"],
